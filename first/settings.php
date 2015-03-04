2aaaa887f62cfae10f3c9db7d825f708c0ceae8e
Emir <emiratesbp@gmail.com>
2015-03-04 14:30:41 +0100

    First commit

---
+++ first/settings.php
@@ -0,0 +1,8 @@
+<?php
+	$adresa="1001 Ogden Ave. Suite 101"; //jako je vazno navesti dobro adresu radi boljeg poziciniranja
+	$grad="Downers Grove, Illinois 60515";//jako je vazno ako adresa ima dva imena povezati ih sa plus
+	$skracenica=""; // AKo ne znate skracenicu ostavite praznu
+	$telephone=6309690402;// Broj koji se poziva na dugme CALL US
+	$theme="b";//Thema koja se aplicira na sve
+	$transitionefect="slidedown"; //kucate tranziciju koju zelite na stranama . Vrste tranzicija : "fade", "flip", "pop", "slide", "slidedown", "slideup"
+?>
